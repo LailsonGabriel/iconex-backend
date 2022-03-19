a490906740773d7f27ef029dff33e44fce76d35d
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-19 20:05:52 -0300

    setup: created models

---
+++ src/database/models/CompanyModel.ts
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize/types";
+import { db } from "../db";
+
+export const CompnayModel = db.define("Companies", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
