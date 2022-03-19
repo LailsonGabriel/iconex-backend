6573c985cb9e577e5543df76bc7541f863c2ec7d
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-19 19:57:54 -0300

    setup: sequelize config

---
+++ src/database/db.ts
@@ -0,0 +1,12 @@
+import { Sequelize } from "sequelize";
+require('dotenv').config();
+
+export const db = new Sequelize(
+  process.env.DB_NAME || 'name',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASS, {
+    host: process.env.DB_HOST || 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+  }
+);
